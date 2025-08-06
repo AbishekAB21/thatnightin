5c7448846c308e7d2b0d724ae56538fc489f7d01
AbishekAB21 <abishekab1805@outlook.com>
2025-08-06 18:20:04 +0530

    safe area bg zoom issue fixed

--- lib/features/welcome_screen/component/welcome_screen_component.dart
+++ lib/features/welcome_screen/component/welcome_screen_component.dart
@@ -12,71 +12,74 @@ class WelcomeScreenComponent extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final color = ref.watch(themeProvider);
-    return Scaffold(
-      backgroundColor: color.defaultOverlayColor,
-
-      body: Container(
-        padding: EdgeInsets.only(
-          top: 10.0,
-          left: 20.0,
-          right: 20.0,
-          bottom: 70.0,
-        ),
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/welcomeScreenCover.jpg'),
-            fit: BoxFit.cover,
-            opacity: 0.65,
+    return SafeArea(
+      bottom: false,
+      child: Scaffold(
+        backgroundColor: color.defaultOverlayColor,
+      
+        body: Container(
+          padding: EdgeInsets.only(
+            top: 10.0,
+            left: 20.0,
+            right: 20.0,
+            bottom: 70.0,
           ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Text(
-              'ThatNightIn',
-              style: Fontstyles.roboto35px(context, ref, true),
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+      
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/welcomeScreenCover.jpg'),
+              fit: BoxFit.cover,
+              opacity: 0.65,
             ),
-            Text(
-              'Strictly footy!',
-              style: Fontstyles.roboto17Bold(context, ref),
-            ),
-
-            SizedBox(height: 10),
-
-            Material(
-              elevation: 6,
-              borderRadius: BorderRadius.circular(40),
-              child: Container(
-                padding: EdgeInsets.all(10.0),
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  gradient: LinearGradient(
-                    begin: AlignmentDirectional.bottomCenter,
-                    end: Alignment.topRight,
-                    tileMode: TileMode.mirror,
-                    colors: [
-                      color.secondaryGradient1,
-                      color.teritiaryColor,
-                      color.secondaryGradient2,
-                    ],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                'ThatNightIn',
+                style: Fontstyles.roboto35px(context, ref, true),
+              ),
+              Text(
+                'Strictly footy!',
+                style: Fontstyles.roboto17Bold(context, ref),
+              ),
+      
+              SizedBox(height: 10),
+      
+              Material(
+                elevation: 6,
+                borderRadius: BorderRadius.circular(40),
+                child: Container(
+                  padding: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    gradient: LinearGradient(
+                      begin: AlignmentDirectional.bottomCenter,
+                      end: Alignment.topRight,
+                      tileMode: TileMode.mirror,
+                      colors: [
+                        color.secondaryGradient1,
+                        color.teritiaryColor,
+                        color.secondaryGradient2,
+                      ],
+                    ),
                   ),
-                ),
-                child: IconButton(
-                  enableFeedback: true,
-                  onPressed: () {
-                    context.pushReplacement('/signin-screen');
-                  },
-                  icon: Icon(
-                    Icons.arrow_forward_ios_rounded,
-                    color: color.iconColor,
+                  child: IconButton(
+                    enableFeedback: true,
+                    onPressed: () {
+                      context.pushReplacement('/signin-screen');
+                    },
+                    icon: Icon(
+                      Icons.arrow_forward_ios_rounded,
+                      color: color.iconColor,
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
